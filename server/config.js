dcd3a029fdd3330de83a986a51160b0169b30445
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-18 11:06:52 -0700

    Created mlab database and set config file DATABASE_URL

--- server/config.js
+++ server/config.js
@@ -1,1 +1,1 @@
-module.exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/graphql-first';
+module.exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://testuser:testuser1@ds261342.mlab.com:61342/guidepost';
