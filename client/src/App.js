2e5247ab33d874b1d8c57cace9312e0ab33fac83
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-20 08:25:21 -0700

    Changed title and intro text

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Advice } from './Advice';
 import { CreateAdvice } from './CreateAdvice';
@@ -28,11 +27,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Guidepost</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Intro Text Here
         </p>
         <CreateAdvice />
         <Advice />
