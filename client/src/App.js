9dcfeb3734766911a7143f40747f371c9fc3e2d4
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-20 09:21:38 -0700

    Built out front end to toglle between api generated advice and custom generated advice

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,56 @@ import { GenerateAdvice } from './GenerateAdvice';
 import { CreateAdvice } from './CreateAdvice';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      customAdviceButton: false
+    }
+  }
+
+  toggleAdviceView(bool) {
+    this.setState({
+      customAdviceButton: bool
+    })
+  }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Guidepost</h1>
-        </header>
-        <p className="App-intro">
-          Your Daily Guidepost
-        </p>
-        <GenerateAdvice />
-        <CreateAdvice />
-        <Advice />
-      </div>
-    );
+    if(!this.state.customAdviceButton) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Guidepost</h1>
+          </header>
+          <p className="App-intro">
+            Your Daily Guidepost
+          </p>
+          <GenerateAdvice />
+          <CreateAdvice />
+          <div className="custom-advice-button">
+            <p>Want to use your own guidepost for todays daily advice?</p>
+            <button type="button" onClick={() => this.toggleAdviceView(true)}>Get Advice!</button>
+          </div>
+        </div>
+      );
+    } else if(this.state.customAdviceButton) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Guidepost</h1>
+          </header>
+          <p className="App-intro">
+            Your Custom Guidepost
+          </p>
+          <Advice />
+          <CreateAdvice />
+          <div className="custom-advice-button">
+            <p>Want to use a randomized guidepost for todays daily advice?</p>
+            <button type="button" onClick={() => this.toggleAdviceView(false)}>Get Advice!</button>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
