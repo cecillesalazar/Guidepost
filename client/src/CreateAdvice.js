e533851709f338b9a24e3ff3da2d057544a03a46
Cecille Salazar <cecillersalazar@gmail.com>
2018-10-18 17:34:10 -0700

    Edited form to clear input field upon post

--- client/src/CreateAdvice.js
+++ client/src/CreateAdvice.js
@@ -8,21 +8,36 @@ export class CreateAdvice extends Component{
   state = {
     customAdvice: ''
   }
+
   render(){
     const { customAdvice } = this.state;
     return (
       <div>
-        <input className="custom-advice-input" type="text" value={ customAdvice } onChange={e => this.setState({customAdvice: e.target.value})} placeholder="The early bird..."/>
-        <Mutation mutation={CREATE_ADVICE} variables={{ customAdvice }} update={(store, { data: { post } }) => {
-          const state = store.readQuery({ query: FEED_QUERY });
-          const newState = [...state.feed, post];
-          store.writeQuery({
-            query: FEED_QUERY,
-            data: {feed: newState}
-          });
-        }}>
-          {createAdvice => <button className="publish-button" onClick={createAdvice}>Publish</button>}
-        </Mutation>
+          <form onSubmit={e => {
+            e.preventDefault();
+            this.inputText.value='';
+          }}>
+          <input
+            className="custom-advice-input"
+            name="input"
+            type="text" value={ customAdvice }
+            onChange={e => {
+              this.setState({customAdvice: e.target.value})
+            }}
+            placeholder="The early bird..."
+            ref={input => this.inputText = input}
+          />
+          <Mutation mutation={CREATE_ADVICE} variables={{ customAdvice }} update={(store, { data: { post } }) => {
+            const state = store.readQuery({ query: FEED_QUERY });
+            const newState = [...state.feed, post];
+            store.writeQuery({
+              query: FEED_QUERY,
+              data: {feed: newState}
+            });
+          }}>
+            {createAdvice => <button type="submit" className="publish-button" onSubmit={createAdvice}>Publish</button>}
+          </Mutation>
+        </form>
       </div>
     )
   }
