e0409ad34cf2917c817ae80bdcc200d063176c39
Cecille Salazar <cecillersalazar@gmail.com>
2018-10-18 17:20:28 -0700

    Fixed the bug on the post - removed authentication for now

--- server/mutations/post.js
+++ server/mutations/post.js
@@ -1,7 +1,7 @@
 const { getJWTPayload } = require('../utils');
 
 module.exports = async (root, args, context) => {
-  const { id } = getJWTPayload(context);
+  const { id } = context;
   return await context.adviceModel.create({
     customAdvice: args.customAdvice,
     author: id
